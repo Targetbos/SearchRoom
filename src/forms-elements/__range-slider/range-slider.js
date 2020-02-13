dd529ffc67719edf7edc72cc3ba7926e7a887049
artdisney <targetbos@yandex.ru>
2020-02-13 12:47:38 +0700

    Add range-slider

---
+++ src/forms-elements/__range-slider/range-slider.js
@@ -0,0 +1,41 @@
+// var rangeBlock = document.querySelector('.range-slider__block');
+// var rangeMin = document.querySelector('.range-slider__min');
+// var rangeBody = document.querySelector('.range-slider__body');
+// var rangeMax = document.querySelector('.range-slider__max');
+// var widthBlock = rangeBlock.offsetWidth; // ширина блока слайдера
+// console.log(widthBlock);
+// let btn = rangeMin.getBoundingClientRect();
+//   console.log(btn);
+// // rangeMin.onmousedown = function(el){
+//   let btn = el.getBoundingClientRect();
+//   console.log(btn);
+// }
+// if (rangeMin.onmousedown()){
+//   var isDown = true;
+//   console.log('true')
+// } else {
+//   isDown = false; 
+// }
+// while(isDown == true){ 
+//   if (rangeMin.offsetLeft > 0 && rangeMin.offsetLeft < widthBlock){
+//     rangeMin.onmousemove = function(el){
+//       let pozCursor = el.layerX;
+//       if (el.layerX > pozCursor){
+//         rangeMin.offsetLeft += el.layerX - pozCursor; 
+//         console.log(rangeMin.offsetLeft);
+//       } else {
+//       }
+//     }
+//     console.log(el.layerX);
+//   }
+// }
+
+
+
+rangeBlock.onmousemove = function(el){
+  let pozBlock = el.layerX;
+  let maxWidthBlock = el.offsetWidth;
+}
+  
+  
+
--- src/forms-elements/forms-elements.js
+++ src/forms-elements/forms-elements.js
@@ -1,2 +1,3 @@
 import '/forms-elements/__radio-buttons/radio-buttons';
 import '/forms-elements/__text-field/text-field';
+import '/forms-elements/__range-slider/range-slider';
