ab47c6a037f041bb6517bc079d2ae2bebf7a2cfb
artdisney <targetbos@yandex.ru>
2020-02-18 21:22:39 +0700

    Add expandable checkbox list

---
+++ src/forms-elements/__expandable-checkbox-list/expandable-checkbox-list.js
@@ -0,0 +1,19 @@
+var checkboxOverlay = document.querySelector('.checkbox-list__overlay');
+var checkboxIcon = document.querySelectorAll('.checkbox-list__icon');
+var checkboxItem = document.querySelector('.checkbox-list__item');
+
+document.querySelectorAll('.checkbox-list__wrap').forEach(function(event){
+  event.addEventListener('click', function(el){
+    console.log(event)
+    checkboxItem.classList.add('checkbox-list__item--visible');
+    checkboxIcon[1].classList.add('checkbox-list__icon--rotate');
+    checkboxOverlay.classList.add('checkbox-list__overlay--open');
+  });
+  
+  checkboxOverlay.addEventListener('click', function(elem){
+    checkboxItem.classList.remove('checkbox-list__item--visible');
+    checkboxIcon[1].classList.remove('checkbox-list__icon--rotate');
+    checkboxOverlay.classList.remove('checkbox-list__overlay--open');
+    elem.stopPropagation();
+  });
+});
--- src/forms-elements/forms-elements.js
+++ src/forms-elements/forms-elements.js
@@ -2,3 +2,5 @@ import '/forms-elements/__radio-buttons/radio-buttons';
 import '/forms-elements/__text-field/text-field';
 import '/forms-elements/__range-slider/range-slider';
 import '/forms-elements/__dropdown/dropdown';
+import '/forms-elements/__expandable-checkbox-list/expandable-checkbox-list';
+
