c4aa4cbadf251cc7a351f2ebf51f926241b4883c
artdisney <targetbos@yandex.ru>
2020-02-25 22:12:47 +0700

    Code refactoring in block radio-buttons

--- src/forms-elements/forms-elements.js
+++ src/forms-elements/forms-elements.js
@@ -1,4 +1,3 @@
-import '/forms-elements/__radio-buttons/radio-buttons';
 import '/forms-elements/__range-slider/range-slider';
 import '/forms-elements/__dropdown/__dropdown-room/dropdown-room';
 import '/forms-elements/__dropdown/__dropdown-guest/dropdown-guest';
