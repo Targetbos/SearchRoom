a74f63732c6e0d65a8924a2fe14422a71916e3aa
Andry Smith <targetbos@yandex.ru>
2020-09-14 11:14:47 +0700

    Complete update LandingPage pages

--- src/dist/lp.dev.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-require("./modules/cards/__search-room/search-room");
--- src/modules/formelements/__date-dropdown/date-dropdown.js
+++ src/modules/formelements/__date-dropdown/date-dropdown.js
@@ -6,12 +6,14 @@ import "./sass/navigation.scss";
 import "./sass/timepicker.scss";
 import "./_date-dropdown.scss";
 
-$(".date-dropdown__subtitle").click(function(){
-  $(".datepicker-here").datepicker({
-    inline:true,
-    autoClose:true,
-    navTitles: {
-      days: "MM yyyy"
-    }
-  });
+var dateDropdown = $(".js__datePicker").datepicker({
+  range: true,
+  autoClose: true,
+  navTitles: {
+    days: "MM yyyy"
+  },
+  onSelect: function (fd) {
+    $(".date-dropdown__in-day").val(fd.split(',')[0])
+    $(".date-dropdown__out-day").val(fd.split(',')[1])
+  }
 });
